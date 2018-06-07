dadf9f70a36e15209f5cb827bb0b3381fb817905
JaredAhaza <jaredahazq@gmail.com>
2018-06-07 23:16:53 +0300

    created urls for gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_gallery,name = 'pastGallery'),
+    url('^$',views.news_of_day,name='newsToday'),
 ]
