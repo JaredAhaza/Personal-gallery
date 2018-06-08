cfb3fb76dfbe1d8ec7c7762618759ea30487aa50
JaredAhaza <jaredahazq@gmail.com>
2018-06-08 20:36:23 +0300

    Created Database

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField (max_length = 30, blank=True)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls, id, category, update):
+        updated = cls.objects.filter(id=id).update(category=update)
+        return updated
+
+    def __str__(self):
+        return self.name
