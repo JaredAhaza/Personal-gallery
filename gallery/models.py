58a8a8ff45fa1f3892a008078dd3e1a4bec02b99
JaredAhaza <jaredahazq@gmail.com>
2018-06-09 12:40:53 +0300

    Created image class

--- gallery/models.py
+++ gallery/models.py
@@ -17,3 +17,74 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='gallery/', blank=True)
+    image_url = models.TextField(blank=True)
+    image_name = models.CharField(max_length=30, blank=True)
+    description = models.TextField(max_length=100, blank=True)
+    category = models.ManyToManyField(Category, blank=True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    locaton = models.ForeignKey('Location', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-post_date']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls, id, target, update):
+        updated = cls.objects.filter(id=id).update(target=update)
+        return updated
+
+    @classmethod
+    def get_all(cls):
+        images = cls.objects.order_by('-post_date')
+        return images
+
+    @classmethod
+    def get_image(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def searched(cls, query):
+        result = cls.objects.filter(description_icontains=query).order_by('-post_date')
+
+        return result
+
+    @classmethod
+    def today_pics(cls):
+        today = dt.date.today()
+        images = cls.objects.filter(post_date__date=today)
+        return images
+
+
+    @classmethod
+    def get_cars(cls):
+        cat_images = cls.objects.filter(
+            category__name__startswith='cars').order_by('-post_date')
+        return cat_images
+
+
+    @classmethod
+    def myhood(cls):
+        images = cls.objects.filter(
+            location__location__startswith='myhood').order_by('-post_date')
+        return images
+
+    @classmethod
+    def videogames(cls):
+        images = cls.objects.filter(
+            location__location__startswith='videogames').order_by('-post_date')
+        return images
+
+    @classmethod
+    def personal(cls):
+        images = cls.objects.filter(
+            location__location__startswith='personal').order_by('-post_date')
+        return images
